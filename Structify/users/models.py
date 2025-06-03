db8c5b1d9ddfeb07a56dfe05cdbdb47fae3dc217
Hitesh Jethava <hitesh.jethava@simformsolutions.com>
2025-06-03 19:52:26 +0530

    Update

--- Structify/users/models.py
+++ Structify/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+
+
+class Role(models.TextChoices):
+    ADMIN = "ADMIN", "Admin"
+    MANAGER = "MANAGER", "Manager"
+    EMPLOYEE = "EMPLOYEE", "Employee"
+
+
+class CustomUser(AbstractBaseUser):
+    pass
