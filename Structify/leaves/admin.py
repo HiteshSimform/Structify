5f5ab55a6adffe16ae8ebec789f18fb51fb4ffe8
Hitesh Jethava <hitesh.jethava@simformsolutions.com>
2025-06-05 11:44:10 +0530

    Structify - Added models in Admin panel

--- Structify/leaves/admin.py
+++ Structify/leaves/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import LeaveApplication, LeaveBalance, LeaveType
 # Register your models here.
+admin.site.register(LeaveApplication)
+admin.site.register(LeaveBalance)
+admin.site.register(LeaveType)
